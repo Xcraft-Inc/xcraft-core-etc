493bda2ae3a3971c71c729898fefa1ab9d68d9af
Samuel Loup <samuel.loup@gmail.com>
2014-10-09 21:42:39 +0200

    autoconfigure example on postinstall for xcraft modules

--- index.js
+++ index.js
@@ -3,18 +3,31 @@
 var moduleName = 'config';
 
 var path       = require ('path');
+var fs         = require ('fs');
+var xFs        = require ('xcraft-core-fs');
 var zogLog     = require ('xcraft-core-log') (moduleName);
 var etcPath    = null;
 
-module.export = function (etcRoot) {
-  etcPath = path.normalize (etcRoot);
+
+module.exports = function (configFilePath) {
+  etcPath = path.normalize (configFilePath);
+  xFs.mkdir (etcPath);
+  zogLog.info ('Initialized config directory at ' + etcPath);
+  
   return {
-    createDefaultConfigFile : function (packageName) {
-      zogLog.info ('Create config file for ' +
-                   packageName +
-                   ' in ' +
-                   etcPath
-                  );
+    createDefaultConfigFile : function (config) {
+      zogLog.info ('Create config file in ' + etcPath);
+
+      var defaultConfig = {};
+      var fileName      = path.join (etcPath, 'config.json');
+      config.forEach (function(def) {
+        if (def.hasOwnProperty ('default') ) {
+          defaultConfig[def.name] = def.default;
+        }
+      });
+
+      zogLog.info (JSON.stringify (defaultConfig));
+      fs.writeFileSync (fileName, JSON.stringify (defaultConfig));
     },
     loadConfigFileForPackage : function (packageName) {
       zogLog.info ('Load config file from ' + path.join (etcPath, packageName));

ba3e7793a1b05ebd7a00f9855f71f017a58550a9
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-10-11 16:32:24 +0200

    The configuration must not be passed with the commands because it is the responsability of every package to retrieve properly they own config with xcraft-core-etc.

--- index.js
+++ index.js
@@ -6,62 +6,60 @@ var path       = require ('path');
 var fs         = require ('fs');
 var xFs        = require ('xcraft-core-fs');
 var zogLog     = require ('xcraft-core-log') (moduleName);
-var etcPath    = null;
+
+/* FIXME: look for a better way in order to retrieve the main etc/ directory. */
+var etcPath    = path.resolve (__dirname, '../../etc/');
+
 
 /**
- * Initialize the main configuration directory.
- * @param {string} configFilePath - Path on etc/ directory.
+ * Create the config file for a specific module.
+ * A subdirectory is created for the module.
+ * @param {Object} config - The Inquirer definition.
+ * @param {string} moduleName
  */
-module.exports = function (configFilePath) {
-  etcPath = path.normalize (configFilePath);
-  xFs.mkdir (etcPath);
-  zogLog.info ('Initialized main config directory at ' + etcPath);
+exports.createDefaultConfigFile = function (config, moduleName) {
+  var moduleEtc = path.resolve (etcPath, moduleName);
+  xFs.mkdir (moduleEtc);
 
-  /**
-   * Create the config file for a specific module.
-   * A subdirectory is created for the module.
-   * @param {Object} config - The Inquirer definition.
-   * @param {string} moduleName
-   */
-  var createConfigFile = function (config, moduleName) {
-    var moduleEtc = path.resolve (etcPath, moduleName);
-    xFs.mkdir (moduleEtc);
+  zogLog.info ('Create config file in ' + moduleEtc);
 
-    zogLog.info ('Create config file in ' + moduleEtc);
+  var defaultConfig = {};
+  var fileName = path.join (moduleEtc, 'config.json');
 
-    var defaultConfig = {};
-    var fileName = path.join (moduleEtc, 'config.json');
-
-    config.forEach (function (def) {
-      if (def.hasOwnProperty ('default') ) {
-        defaultConfig[def.name] = def.default;
-      }
-    });
-
-    zogLog.verb (JSON.stringify (defaultConfig));
-    fs.writeFileSync (fileName, JSON.stringify (defaultConfig, null, '  '));
-  };
-
-  return {
-    createDefaultConfigFile: createConfigFile,
-
-    createAllConfigFiles: function (modulePath, filterRegex) {
-      var path  = require ('path');
-      var zogFs = require ('xcraft-core-fs');
-
-      var xModulesFiles = zogFs.ls (modulePath, filterRegex);
-
-      xModulesFiles.forEach (function (fileName) {
-        var xModule = require (path.join (modulePath, fileName));
-
-        if (xModule.hasOwnProperty ('xcraftConfig')) {
-          createConfigFile (xModule.xcraftConfig, fileName);
-        }
-      });
-    },
-
-    loadConfigFileForPackage: function (packageName) {
-      zogLog.info ('Load config file from ' + path.join (etcPath, packageName));
+  config.forEach (function (def) {
+    if (def.hasOwnProperty ('default') ) {
+      defaultConfig[def.name] = def.default;
     }
-  };
+  });
+
+  zogLog.verb (JSON.stringify (defaultConfig));
+  fs.writeFileSync (fileName, JSON.stringify (defaultConfig, null, '  '));
+};
+
+exports.createAllConfigFiles = function (modulePath, filterRegex) {
+  var path  = require ('path');
+  var zogFs = require ('xcraft-core-fs');
+
+  var xModulesFiles = zogFs.ls (modulePath, filterRegex);
+
+  xModulesFiles.forEach (function (fileName) {
+    var xModule = require (path.join (modulePath, fileName));
+
+    if (xModule.hasOwnProperty ('xcraftConfig')) {
+      exports.createDefaultConfigFile (xModule.xcraftConfig, fileName);
+    }
+  });
+};
+
+exports.loadConfigFileForPackage = function (packageName) {
+  var configFile = path.join (etcPath, packageName, 'config.json');
+
+  zogLog.verb ('Load config file from ' + configFile);
+
+  /* FIXME: handle fallback to the internal package config entries. */
+  try {
+    return JSON.parse (fs.readFileSync (configFile, 'utf8'));
+  } catch (err) {
+    return null;
+  }
 };

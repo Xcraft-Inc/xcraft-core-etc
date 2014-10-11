c711b5608c3b33192757d359a55d802014c6808b
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-10-11 14:59:17 +0200

    Just after the publish (in stage1), then we create all configuration files in etc/. Note that the .zogrc file is not handled here because this one will be change to an xcraft.json file and it will be handled by an other way.

--- index.js
+++ index.js
@@ -8,28 +8,59 @@ var xFs        = require ('xcraft-core-fs');
 var zogLog     = require ('xcraft-core-log') (moduleName);
 var etcPath    = null;
 
-
+/**
+ * Initialize the main configuration directory.
+ * @param {string} configFilePath - Path on etc/ directory.
+ */
 module.exports = function (configFilePath) {
   etcPath = path.normalize (configFilePath);
   xFs.mkdir (etcPath);
-  zogLog.info ('Initialized config directory at ' + etcPath);
+  zogLog.info ('Initialized main config directory at ' + etcPath);
+
+  /**
+   * Create the config file for a specific module.
+   * A subdirectory is created for the module.
+   * @param {Object} config - The Inquirer definition.
+   * @param {string} moduleName
+   */
+  var createConfigFile = function (config, moduleName) {
+    var moduleEtc = path.resolve (etcPath, moduleName);
+    xFs.mkdir (moduleEtc);
+
+    zogLog.info ('Create config file in ' + moduleEtc);
+
+    var defaultConfig = {};
+    var fileName = path.join (moduleEtc, 'config.json');
+
+    config.forEach (function (def) {
+      if (def.hasOwnProperty ('default') ) {
+        defaultConfig[def.name] = def.default;
+      }
+    });
+
+    zogLog.verb (JSON.stringify (defaultConfig));
+    fs.writeFileSync (fileName, JSON.stringify (defaultConfig, null, '  '));
+  };
 
   return {
-    createDefaultConfigFile : function (config) {
-      zogLog.info ('Create config file in ' + etcPath);
+    createDefaultConfigFile: createConfigFile,
 
-      var defaultConfig = {};
-      var fileName      = path.join (etcPath, 'config.json');
-      config.forEach (function (def) {
-        if (def.hasOwnProperty ('default') ) {
-          defaultConfig[def.name] = def.default;
+    createAllConfigFiles: function (modulePath, filterRegex) {
+      var path  = require ('path');
+      var zogFs = require ('xcraft-core-fs');
+
+      var xModulesFiles = zogFs.ls (modulePath, filterRegex);
+
+      xModulesFiles.forEach (function (fileName) {
+        var xModule = require (path.join (modulePath, fileName));
+
+        if (xModule.hasOwnProperty ('xcraftConfig')) {
+          createConfigFile (xModule.xcraftConfig, fileName);
         }
       });
-
-      zogLog.info (JSON.stringify (defaultConfig));
-      fs.writeFileSync (fileName, JSON.stringify (defaultConfig));
     },
-    loadConfigFileForPackage : function (packageName) {
+
+    loadConfigFileForPackage: function (packageName) {
       zogLog.info ('Load config file from ' + path.join (etcPath, packageName));
     }
   };

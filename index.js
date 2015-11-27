097f6d104161f83b842a9ce82cd303b80936b99c
Mathieu Schroeter <schroeter@epsitec.ch>
2015-11-27 14:50:25 +0100

    Fix the way to retrieve the root directory since node > 0.x
    With latest node version, there is a new npm which prefer to install the
    dependencies in a flat tree. It's the main problem for this regression.

--- index.js
+++ index.js
@@ -10,15 +10,12 @@ var xLog = require ('xcraft-core-log') (moduleName);
 
 var confCache  = {};
 
-var etcPath    = path.resolve (__dirname, '../../etc/');
+var dirArray = __dirname.split (path.sep);
+var pos = dirArray.indexOf ('toolchain');
+var toolChainDir = path.resolve (__dirname, dirArray.slice (0, pos + 1).join (path.sep));
+var etcPath = path.join (toolChainDir, 'etc');
 if (!fs.existsSync (etcPath)) {
-  var dirArray = __dirname.split (path.sep);
-  var pos = dirArray.indexOf ('toolchain');
-  var toolChainDir = path.resolve (__dirname, dirArray.slice (0, pos + 1).join (path.sep));
-  etcPath = path.join (toolChainDir, 'etc');
-  if (!fs.existsSync (etcPath)) {
-    xLog.err ('root etc cannot be resolved ! are you in the toolchain ?');
-  }
+  xLog.err ('${root}/etc cannot be resolved! Are you in the toolchain?');
 }
 
 /**

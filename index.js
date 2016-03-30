b475cc125e503f77e19ced400e104e38b539957c
Mathieu Schroeter <schroeter@epsitec.ch>
2016-03-30 14:56:06 +0200

    Rewrite xEtc as a singleton in order to be relocatable

--- index.js
+++ index.js
@@ -8,128 +8,148 @@ var fs   = require ('fs');
 var xFs  = require ('xcraft-core-fs');
 var xLog = require ('xcraft-core-log') (moduleName);
 
-var confCache  = {};
+let etcInstance = null;
 
-var dirArray = __dirname.split (path.sep);
-var pos = dirArray.indexOf ('toolchain');
-var toolChainDir = path.resolve (__dirname, dirArray.slice (0, pos + 1).join (path.sep));
-var etcPath = path.join (toolChainDir, 'etc');
-if (!fs.existsSync (etcPath)) {
-  xLog.err ('${root}/etc cannot be resolved! Are you in the toolchain?');
-}
+class Etc {
+  constructor (root) {
+    this.confCache  = {};
 
-/**
- * Create the config file for a specific module.
- * A subdirectory is created for the module.
- *
- * @param {Object} config - The Inquirer definition.
- * @param {string} moduleName
- */
-exports.createDefault = function (config, moduleName) {
-  var moduleEtc = path.resolve (etcPath, moduleName);
-  xFs.mkdir (moduleEtc);
-
-  xLog.info ('Create config file in ' + moduleEtc);
-
-  var defaultConfig = {};
-  var fileName = path.join (moduleEtc, 'config.json');
-
-  config.forEach (function (def) {
-    if (def.hasOwnProperty ('default') ) {
-      defaultConfig[def.name] = def.default;
-    }
-  });
-
-  xLog.verb (JSON.stringify (defaultConfig));
-  fs.writeFileSync (fileName, JSON.stringify (defaultConfig, null, '  '));
-};
-
-exports.createAll = function (modulePath, filterRegex) {
-  var path = require ('path');
-  var xFs  = require ('xcraft-core-fs');
-
-  var xModulesFiles = xFs.ls (modulePath, filterRegex);
-
-  xModulesFiles.forEach (function (mod) {
-    var xModule = null;
-    try {
-      xModule = require (path.join (modulePath, mod, 'config.js'));
-    } catch (ex) {
-      return;
+    if (!root) {
+      const dirArray = __dirname.split (path.sep);
+      const pos = dirArray.indexOf ('toolchain');
+      const toolChainDir = path.resolve (__dirname, dirArray.slice (0, pos + 1).join (path.sep));
+      this.etcPath = path.join (toolChainDir, 'etc');
+    } else {
+      this.etcPath = root;
     }
 
-    exports.createDefault (xModule, mod);
-  });
-};
-
-exports.configureAll = function (modulePath, filterRegex, wizCallback) {
-  var async = require ('async');
-  var path  = require ('path');
-  var xFs   = require ('xcraft-core-fs');
-  var wizards = {};
-
-  var xModulesFiles = xFs.ls (modulePath, filterRegex);
-
-  xModulesFiles.forEach (function (mod) {
-    var xModule = null;
-    try {
-      xModule = require (path.join (modulePath, mod, 'config.js'));
-    } catch (ex) {
-      return;
+    if (!fs.existsSync (this.etcPath)) {
+      xLog.err ('${root}/etc cannot be resolved! Are you in the toolchain?');
     }
+  }
 
-    wizards[mod] = xModule;
+  /**
+   * Create the config file for a specific module.
+   * A subdirectory is created for the module.
+   *
+   * @param {Object} config - The Inquirer definition.
+   * @param {string} moduleName
+   */
+  createDefault (config, moduleName) {
+    var moduleEtc = path.resolve (this.etcPath, moduleName);
+    xFs.mkdir (moduleEtc);
 
-    /* Retrieve the current values if possible. */
-    try {
-      var configFile = path.join (etcPath, mod, 'config.json');
-      var data = JSON.parse (fs.readFileSync (configFile, 'utf8'));
+    xLog.info ('Create config file in ' + moduleEtc);
 
-      wizards[mod].forEach (function (item, index) {
-        wizards[mod][index].default = data[item.name];
-      });
-    } catch (ex) {}
-  });
+    var defaultConfig = {};
+    var fileName = path.join (moduleEtc, 'config.json');
 
-  async.eachSeries (Object.keys (wizards), function (wiz, callback) {
-    xLog.info ('configure Xcraft (%s)', wiz);
-    wizCallback (wizards[wiz], function (answers) {
-      var hasChanged = false;
+    config.forEach (function (def) {
+      if (def.hasOwnProperty ('default') ) {
+        defaultConfig[def.name] = def.default;
+      }
+    });
 
-      xLog.verb ('JSON output:\n' + JSON.stringify (answers, null, '  '));
+    xLog.verb (JSON.stringify (defaultConfig));
+    fs.writeFileSync (fileName, JSON.stringify (defaultConfig, null, '  '));
+  }
 
-      Object.keys (answers).forEach (function (item) {
-        if (wizards[wiz][item] !== answers[item]) {
-          wizards[wiz][item] = answers[item];
-          hasChanged = true;
-        }
-      });
+  createAll (modulePath, filterRegex) {
+    var path = require ('path');
+    var xFs  = require ('xcraft-core-fs');
 
-      if (hasChanged) {
-        var configFile = path.join (etcPath, wiz, 'config.json');
-        fs.writeFileSync (configFile, JSON.stringify (answers, null, '  '));
+    var xModulesFiles = xFs.ls (modulePath, filterRegex);
+
+    xModulesFiles.forEach ((mod) => {
+      var xModule = null;
+      try {
+        xModule = require (path.join (modulePath, mod, 'config.js'));
+      } catch (ex) {
+        return;
       }
 
-      callback ();
+      this.createDefault (xModule, mod);
     });
-  }, function () {
-    wizCallback ();
-  });
-};
-
-exports.load = function (packageName) {
-  var configFile = path.join (etcPath, packageName, 'config.json');
-
-  /* FIXME: handle fallback to the internal package config entries. */
-  try {
-    if (confCache[packageName] === undefined) {
-      xLog.verb ('Load config file from ' + configFile);
-      confCache[packageName] = JSON.parse (fs.readFileSync (configFile, 'utf8'));
-      return confCache[packageName];
-    } else {
-      return confCache[packageName];
-    }
-  } catch (ex) {
-    return null;
   }
+
+  configureAll (modulePath, filterRegex, wizCallback) {
+    var async = require ('async');
+    var path  = require ('path');
+    var xFs   = require ('xcraft-core-fs');
+    var wizards = {};
+
+    var xModulesFiles = xFs.ls (modulePath, filterRegex);
+
+    xModulesFiles.forEach (function (mod) {
+      var xModule = null;
+      try {
+        xModule = require (path.join (modulePath, mod, 'config.js'));
+      } catch (ex) {
+        return;
+      }
+
+      wizards[mod] = xModule;
+
+      /* Retrieve the current values if possible. */
+      try {
+        var configFile = path.join (this.etcPath, mod, 'config.json');
+        var data = JSON.parse (fs.readFileSync (configFile, 'utf8'));
+
+        wizards[mod].forEach (function (item, index) {
+          wizards[mod][index].default = data[item.name];
+        });
+      } catch (ex) {}
+    });
+
+    async.eachSeries (Object.keys (wizards), function (wiz, callback) {
+      xLog.info ('configure Xcraft (%s)', wiz);
+      wizCallback (wizards[wiz], function (answers) {
+        var hasChanged = false;
+
+        xLog.verb ('JSON output:\n' + JSON.stringify (answers, null, '  '));
+
+        Object.keys (answers).forEach (function (item) {
+          if (wizards[wiz][item] !== answers[item]) {
+            wizards[wiz][item] = answers[item];
+            hasChanged = true;
+          }
+        });
+
+        if (hasChanged) {
+          var configFile = path.join (this.etcPath, wiz, 'config.json');
+          fs.writeFileSync (configFile, JSON.stringify (answers, null, '  '));
+        }
+
+        callback ();
+      });
+    }, function () {
+      wizCallback ();
+    });
+  }
+
+  load (packageName) {
+    var configFile = path.join (this.etcPath, packageName, 'config.json');
+
+    /* FIXME: handle fallback to the internal package config entries. */
+    try {
+      if (this.confCache[packageName] === undefined) {
+        xLog.verb ('Load config file from ' + configFile);
+        this.confCache[packageName] = JSON.parse (fs.readFileSync (configFile, 'utf8'));
+        return this.confCache[packageName];
+      } else {
+        return this.confCache[packageName];
+      }
+    } catch (ex) {
+      return null;
+    }
+  }
+}
+
+module.exports = (root) => {
+  if (etcInstance) {
+    return etcInstance;
+  }
+
+  etcInstance = new Etc (root);
+  return etcInstance;
 };
